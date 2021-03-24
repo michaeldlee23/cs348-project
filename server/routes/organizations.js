4bcc3bf4b07d94a0c92a46116cee45f73b06d7ce
Xinyi Xiao <54774423+xiao236@users.noreply.github.com>
2021-03-23 22:16:32 -0400

    Create organizations.js

---
+++ server/routes/organizations.js
@@ -0,0 +1,83 @@
+'use strict';
+
+const pool = require('../connection');
+const validation = require('../validation');
+const jsonConverter = require('../util/jsonConverter');
+
+const ENDPOINT = '/organizations';
+const ENTITY = 'organizations';
+
+module.exports = (app) => {
+    app.get(ENDPOINT, (req, res) => {
+        const ERR_MESSAGE = 'Failed to retrieve organizations';
+        const sql = `SELECT * FROM ${ENTITY}`;
+        pool.query(sql, (err, results) => {
+            if (err) {
+                return res.status(500).json({
+                    message: ERR_MESSAGE,
+                    data: err.message
+                });
+            }
+            return res.status(200).json(results);
+        });
+    });
+
+    app.post(ENDPOINT, (req, res) => {
+        const ERR_MESSAGE = 'Failed to add organizations';
+        const SUC_MESSAGE = 'Successfully added organizations';
+        const payload = req.body;
+        const err = validation.request.postOrganizationSchema.validate(payload).error;
+        if (err) {
+            return res.status(400).json({
+                message: ERR_MESSAGE,
+                data: err.message
+            });
+        }
+        const sql = `INSERT INTO ${ENTITY}(${Object.keys(payload).toString()}) VALUES (?)`;
+        pool.query(sql, [Object.values(payload)], async (err) => {
+            if (err) {
+                return res.status(500).json({
+                    message: ERR_MESSAGE,
+                    data: err.message
+                });
+            }
+            return res.status(200).json({
+                message: SUC_MESSAGE,
+                data: payload
+            });
+        });
+    });
+
+    app.put(ENDPOINT, (req, res) => {
+        const ERR_MESSAGE = 'Failed to update organization record';
+        const SUC_MESSAGE = 'Successfully updated organization record';
+        const payload = req.body;
+        const err = validation.request.putOrganizationSchema.validate(payload).error;
+        if (err) {
+            return res.status(400).json({
+                message: ERR_MESSAGE,
+                data: err.message
+            });
+        }
+        const values = jsonConverter.payloadToUpdate(payload);
+        const sql = `UPDATE ${ENTITY} SET ${values} WHERE id=${payload.id}`;
+        pool.query(sql, async (err, results) => {
+            if (results.changedRows == 0) {
+                return res.status(404).json({
+                    message: ERR_MESSAGE,
+                    data: `No course with id ${payload.id}`
+                });
+            }
+            if (err) {
+                return res.status(500).json({
+                    message: ERR_MESSAGE,
+                    data: err.message
+                });
+            }
+            return res.status(200).json({
+                message: SUC_MESSAGE,
+                data: payload
+            });
+        });
+    });
+}
