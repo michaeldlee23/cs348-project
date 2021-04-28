6d2d755216aeaa6d1980456a0ed7248b97382c82
Winston-Wang-01 <54554850+Winston-Wang-01@users.noreply.github.com>
2021-04-27 21:25:35 -0400

    course creation through admin/addCours

--- server/validation/request/courses/postCourse.js
+++ server/validation/request/courses/postCourse.js
@@ -5,4 +5,5 @@ const joi = require('joi');
 module.exports = joi.object({
     code: joi.string().max(15).required(),
     name: joi.string().max(50).required(),
+    departmentID: joi.number().integer().required()
 });
